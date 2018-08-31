03a5ef19ee887921a90a2a5ee76f005b82f921d3
Sam Tang <samtang1430@gmail.com>
2018-08-31 14:39:55 -0400

    Added annotation to process

--- src/Product.js
+++ src/Product.js
@@ -9,6 +9,10 @@ class Product extends Component{
         return(
             <div className="Product">
                 <div className='top'>
+                    <div className="imageContainer">
+                        <img src={require('./img/product1.png')} />
+                        <img src={require('./img/product2.png')} />
+                    </div>
                     <h1> Dr. Lotus AI</h1>
                     <p>The artificial intelligence-assisted diagnosis system has realized the application of artificial intelligence and high-risk screening for breast cancer, breast image interpretation, rectal imaging interpretation and other artificial intelligence applications. The breast cancer imaging field has reached the world's leading level. </p>
                 </div>
@@ -18,7 +22,23 @@ class Product extends Component{
                     <Feature img = {require('./img/reliable.png')} title='Reliable Data' description='Uses top hospitals data obtained from the diagnoses and guidance of authoritative experts in the industry.'/>
                 </div>
                 <div className = 'process'>
-                    <img src={require('./img/cycle.png')} />
+                    <div className="left" >
+                        <div className="processContainer">
+                            <img src={require('./img/cycle.png')} style={{height:'400px', width:'400px'}}/>
+                            <div data-aos = "fade-right" className = "annotate1">
+                                <div className="text"> Remote visit, green transfer </div>
+                                <div className="line" />
+                            </div>
+                            <div data-aos = "fade-right" className = "annotate2">
+                                <div className="text"> Appointment Reminder Customer Service </div>
+                                <div className="line" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <h2>Diagnosis Made Easy.</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae sapiente vero, dolor officia fuga adipisci fugiat ut iste dolorum quaerat quis quam eos libero? Reiciendis provident suscipit adipisci dignissimos necessitatibus.</p>
+                    </div>
                 </div>
             </div>
         )
